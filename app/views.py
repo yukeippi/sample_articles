0d24abc175ff03d3148acde8da3ec6c8cef8924c
Yuichiro Kei <yuichirokei@gmail.com>
2025-11-09 17:38:44 +0900

    記事のindexページを作成

--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from app.models import Article
+
 
 class IndexView(View):
     def get(self, request):
-        return render(request, 'index.html')
+        articles = Article.objects.select_related('user').all()
+        context = {
+            'articles': articles,
+        }
+        return render(request, 'index.html', context)
