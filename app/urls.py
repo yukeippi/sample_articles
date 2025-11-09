e46aecb9dabf13486892bf0e2ce29db5c68e544e
Yuichiro Kei <yuichirokei@gmail.com>
2025-11-09 19:35:32 +0900

    詳細画面の追加

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,5 @@ app_name = 'app'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('article/<uuid:pk>/', views.ArticleDetailView.as_view(), name='article_detail'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import DetailView, View
 
 from app.models import Article
 
@@ -11,3 +11,12 @@ class IndexView(View):
             'articles': articles,
         }
         return render(request, 'index.html', context)
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+    context_object_name = 'article'
+
+    def get_queryset(self):
+        return Article.objects.select_related('user').prefetch_related('comments__user')
