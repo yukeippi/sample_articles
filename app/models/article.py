89c14ccac86dff87569639826cfcbb4840d368f6
Yuichiro Kei <yuichirokei@gmail.com>
2025-11-09 09:44:40 +0900

    ArticleにUserを紐付け

--- app/models/article.py
+++ app/models/article.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from uuid import uuid7
 from .base import TimestampedModel
 
@@ -8,6 +9,12 @@ class Article(TimestampedModel):
     id = models.UUIDField(primary_key=True, default=uuid7, editable=False, verbose_name='ID')
     title = models.CharField(max_length=200, verbose_name='タイトル')
     content = models.TextField(verbose_name='本文')
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='articles',
+        verbose_name='投稿者'
+    )
 
     class Meta:
         verbose_name = '記事'
